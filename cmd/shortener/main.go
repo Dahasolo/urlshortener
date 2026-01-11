754aabe9d9376666fce8ebbdbe3f0c43db057536
Dahasolo <dahasolo@gmail.com>
2026-01-11 11:52:53 +0300

    feat: replace net/http mux with chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,14 +6,20 @@ import (
 	"github.com/Dahasolo/urlshortener/internal/handler"
 	"github.com/Dahasolo/urlshortener/internal/repository"
 	"github.com/Dahasolo/urlshortener/internal/service"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	repo := repository.NewInMemoryURLRepo()
 	svc := service.NewService(repo)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.ShortenHandler(svc))
-	mux.HandleFunc("/{id}", handler.RedirectHandler(svc))
 
-	http.ListenAndServe(":8080", mux)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", handler.ShortenHandler(svc))
+	// mux.HandleFunc("/{id}", handler.RedirectHandler(svc)) // ← ЭТО НЕ РАБОТАЛО!
+	// http.ListenAndServe(":8080", mux)
+
+	r := chi.NewRouter()
+	r.Post("/", handler.ShortenHandler(svc))
+	r.Get("/{id}", handler.RedirectHandler(svc))
+	http.ListenAndServe(":8080", r)
 }
